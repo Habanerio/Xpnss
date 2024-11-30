955d48df2254c97a68e7c28847812e0e0ffc9c87
Habaner.io <118768882+Habanerio@users.noreply.github.com>
2024-11-29 20:33:29 -0500

    Added `GetUserProfile` Endpoint

---
+++ tests/functional/AppApis/UserProfiles/GetUserProfileApiTests.cs
@@ -0,0 +1,49 @@
+using System.Text.Json;
+using Habanerio.Xpnss.Apis.App.AppApis.Models;
+using Habanerio.Xpnss.Application.DTOs;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Habanerio.Xpnss.Tests.Functional.AppApis.UserProfiles;
+
+public class GetUserProfileApiTests(WebApplicationFactory<Apis.App.AppApis.Program> factory) :
+    BaseFunctionalApisTests(factory),
+    IClassFixture<WebApplicationFactory<Apis.App.AppApis.Program>>
+{
+    private const string ENDPOINTS_USER_PROFILES_GET_USER_PROFILE = "/api/v1/users/{userId}";
+
+    [Fact]
+    public async Task CanCall_GetUserProfile_WithValidRequest_ReturnsOk()
+    {
+        var actualUserProfileDocument = await UserProfileDocumentsRepository
+            .FirstOrDefaultDocumentAsync(u => true);
+
+        if (actualUserProfileDocument is null)
+            Assert.Fail("Cannot find a UserProfileDocument for the test");
+
+        var userId = actualUserProfileDocument.Id;
+
+        // Act
+        var response = await HttpClient.GetAsync(ENDPOINTS_USER_PROFILES_GET_USER_PROFILE
+                .Replace("{userId}", userId.ToString()));
+
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStringAsync();
+        var apiResponse = JsonSerializer.Deserialize<ApiResponse<UserProfileDto>>(
+            content,
+            JsonSerializationOptions);
+
+        // Assert
+        Assert.NotNull(apiResponse);
+        Assert.True(apiResponse.IsSuccess);
+
+        var actualDto = Assert.IsType<UserProfileDto>(apiResponse.Data);
+
+        Assert.NotNull(actualDto);
+        Assert.Equal(userId.ToString(), actualDto.Id);
+        Assert.Equal(actualUserProfileDocument.ExtUserId, actualDto.ExtUserId);
+        Assert.Equal(actualUserProfileDocument.FirstName, actualDto.FirstName);
+        Assert.Equal(actualUserProfileDocument.LastName, actualDto.LastName);
+        Assert.Equal(actualUserProfileDocument.Email, actualDto.Email);
+    }
+}
